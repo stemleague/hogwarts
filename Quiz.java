5bd58905eb73b70a2a7dea05117928ec206d20bd
stem league <64894084+stemleague@users.noreply.github.com>
2021-08-09 14:48:01 -0700

    Create Quiz.java

---
+++ Quiz.java
@@ -0,0 +1,23 @@
+public class Quiz {
+  public static void main(String[] args) {
+    // Question One
+    String favoriteColor = "blue";
+    if (favoriteColor == "blue") {
+      System.out.println("Wise one, you must be a Ravenclaw!");
+    } else if (favoriteColor == "red") {
+      System.out.println("Brave one, you must be a Gryffindor!");
+    } else if (favoriteColor == "green") {
+      System.out.println("Ambitious one, you must be a Slytherin!");
+    } else if (favoriteColor == "yellow") {
+      System.out.println("Extraordinary one, you must be a Hufflepuff!");
+    }
+
+    // Question Two
+    boolean triedOutForQuidditch = true;
+    if (triedOutForQuidditch) {
+      System.out.println("Good luck at the Quidditch try outs!");
+    } else {
+      System.out.println("Excited to cheer on our Quidditch team in the stands!");
+    }
+  }
+}
